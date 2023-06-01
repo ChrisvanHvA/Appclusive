c655c478dbdeeeb00f7fe4a9d77687b834942817
Eva Zaadnoordijk <evazaadnoordijk@gmail.com>
2023-06-01 14:46:23 +0200

    sidebar done

--- build-scripts/buildStyles.js
+++ build-scripts/buildStyles.js
@@ -3,7 +3,12 @@ import concat from 'gulp-concat';
 import cleanCSS from 'gulp-clean-css';
 import autoPrefixer from 'gulp-autoprefixer';
 
-(() => {
+import {deleteAsync} from 'del';
+
+
+( async () => {
+    await deleteAsync('./public/css/index.css');
+
     return gulp
         .src(['./public/css/**/*.css', '!/public/css/index.css'])
         .pipe(concat(`index.css`))
