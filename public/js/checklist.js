1a14f654210a63eeadd7151f532c70d01eb37a13
Sasja Koning <sasjakoning@hotmail.com>
2023-06-06 13:27:31 +0200

    changed: primises to async await

--- public/js/checklist.js
+++ public/js/checklist.js
@@ -1,36 +1,34 @@
 function submitHandler() {
-    console.log('checklist.js')
+    console.log('checklist.js');
 
+    const checklistItemsCheckboxes = document.querySelectorAll(
+        '.checklist__checkbox'
+    );
 
-    const checklistItemsCheckboxes = document.querySelectorAll('.checklist__checkbox');
-
-    checklistItemsCheckboxes.forEach(checkbox => {
-        console.log(checkbox)
-        checkbox.addEventListener('click', (e) => {
-            console.log('clicked')
+    checklistItemsCheckboxes.forEach((checkbox) => {
+        console.log(checkbox);
+        checkbox.addEventListener('click', async (e) => {
+            console.log('clicked');
 
             const checklistValue = e.target.checked ? 1 : 0;
 
-            //  post with fetch
-            fetch('/checklist/submit', {
-                method: 'post',
-                data: { 
-                    wcag_item_id: "",
-                    is_checked: checklistValue 
-                }
-            }).then(res => {
-                return res.json()
-                }).then((data) => {
-                    console.log(data);
-                }
-                ).catch(err => {
-                    console.log(err)
-                })  
-            
-        })
-    })
+            try {
+                const res = await fetch('/checklist/submit', {
+                    method: 'post',
+                    data: {
+                        wcag_item_id: '',
+                        is_checked: checklistValue,
+                    },
+                });
+                const data = await res.json();
+                console.log(data);
+            } catch (err) {
+                console.log(err);
+            }
+        });
+    });
 }
 
 export default {
-    submitHandler
-}
+    submitHandler,
+};
