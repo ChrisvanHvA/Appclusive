a78ee06e8f80a9e80d15fed10fd021f8a8cde057
Laurens256 <duinlaurens2@gmail.com>
2023-06-08 10:13:23 +0200

    improve without js

--- public/js/createProject.js
+++ public/js/createProject.js
@@ -7,8 +7,9 @@ const descriptionCharCount = document.querySelector(
 
 // check if description input and char counter exist, if so, update char counter on input
 if (descriptionInput && descriptionCharCount) {
+	const maxChars = descriptionInput.getAttribute('maxlength') || 500;
     descriptionInput.addEventListener('input', () => {
-        descriptionCharCount.textContent = descriptionInput.value.length;
+        descriptionCharCount.textContent = `${descriptionInput.value.length}/${maxChars}`;
     });
 	// dispatch input event to update char counter on page load
     descriptionInput.dispatchEvent(new Event('input'));
