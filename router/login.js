941aeaf0df5db8559b43a5b56c5ac621909946ab
Eva Zaadnoordijk <evazaadnoordijk@gmail.com>
2023-06-08 12:58:19 +0200

    added: styling after media query

--- router/login.js
+++ router/login.js
@@ -2,7 +2,10 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('login');
+    res.render('login', {
+        noNav: true,
+    });
+
 });
 
 export default router;
