0e2948ad061c97cf025807807562644e52556884
Eva Zaadnoordijk <evazaadnoordijk@gmail.com>
2023-06-08 10:03:13 +0200

    added: css for login and page for login

--- router/router.js
+++ router/router.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import home from './home.js';
 import checklist from './checklist.js';
+import login from './login.js';
 
 const routes = [
+    { path: '/login', view: login },
     { path: '/checklist', view: checklist },
     { path: '/', view: home },
 ];
