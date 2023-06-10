a6d4fd2fe52737d00c8a760aaff82338aa4d44f1
Nina Vens <nina.vens@hva.nl>
2023-06-10 14:05:15 +0200

    added: send mail

--- router/forgotPassword.js
+++ router/forgotPassword.js
@@ -4,11 +4,20 @@ dotenv.config();
 import express from 'express';
 import UserModel from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
+import nodemailer from 'nodemailer';
 
 const router = express.Router();
 const userModel = new UserModel();
 
 const JWT_SECRET = process.env.JWT_SECRET;
+
+const transporter = nodemailer.createTransport({
+    service: process.env.MAILER_SERVICE,
+    auth: {
+        user: process.env.MAILER_MAIL,
+        pass: process.env.MAILER_PASS,
+    },
+});
 
 router.get('/', (req, res, next) => {
     res.render('forgotPassword', { noNav: true });
@@ -32,8 +41,23 @@ router.post('/', async (req, res, next) => {
         };
         const token = jwt.sign(payload, secret, { expiresIn: '15m' });
         const link = `http://localhost:5500/reset-password/${user.user_id}/${token}`;
-    
-        console.log(link);
+
+        const mailOptions = {
+            from: process.env.MAILER_MAIL,
+            to: user.email_address,
+            subject: 'Password Reset',
+            text: `Click the link to reset your password: ${link}`,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error);
+                res.send('Error sending password reset email');
+            } else {
+                console.log('Email sent:', info.response);
+                res.send('Password reset link has been sent to your email');
+            }
+        });
         res.send('Password reset link has been sent to your email');
     } catch (error) {
         console.log(error);
