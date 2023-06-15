3a7e920b1e36dd5e3d416a40d7206f70be777f71
Laurens256 <duinlaurens2@gmail.com>
2023-06-15 13:47:59 +0200

    add: category page to checklist flow

--- router/checklistCategories.js
+++ router/checklistCategories.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import WCAGModel from '../models/wcagModel.js';
 import wcagModel from '../models/wcagModel.js';
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 
 router.get('/', async (req, res) => {
+	const projectId = req.params.projectId;
+
+	if (!projectId || projectId == 0) {
+		// todo: betere redirect / error handling
+		return res.redirect('/');
+	}
+
     const WCAGModel = new wcagModel();
     const categories = await WCAGModel.listWCAGCategories();
 
     res.render('checklistCategories', {
+        ...res.locals,
         categories,
-        ...res.locals
+		projectId
     });
 });
 
