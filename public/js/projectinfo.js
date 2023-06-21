42d1640af5ed93508b367d130b6b82b782b719a7
Laurens256 <duinlaurens2@gmail.com>
2023-06-21 14:40:11 +0200

    wip sidebar without js

--- public/js/projectinfo.js
+++ public/js/projectinfo.js
@@ -1,6 +1,6 @@
 const infoButton = document.querySelector(".sidebar__infoicon");
 const projectInfo = document.querySelector(".checklist__sidebar");
 
-infoButton.addEventListener("click", () => {
+infoButton?.addEventListener("click", () => {
     projectInfo.classList.toggle("checklist__sidebar-active");
 });
