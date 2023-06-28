f8b37c73c7c0ced84c70a7e41371e2ec65ec7e49
Laurens256 <duinlaurens2@gmail.com>
2023-06-28 23:37:26 +0200

    change: better autocomplete code

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -1,134 +1,128 @@
-const autocomplete = (input, array) => {
-    let currentFocus;
+const suggestions = new Set();
 
-    input?.addEventListener('input', function (e) {
-        let autocompleteList,
-            autocompleteItem,
-            i,
-            value = this.value;
-
-        closeAllLists();
-        if (!value) return false;
-        currentFocus = -1;
-
-        autocompleteList = document.createElement('ul');
-        autocompleteList.setAttribute('id', this.id + 'AutocompleteList');
-        autocompleteList.setAttribute('class', 'autocompleteItems');
-
-        this.parentNode.appendChild(autocompleteList);
-
-        for (i = 0; i < array.length; i++) {
-            if (array[i].toUpperCase().indexOf(value.toUpperCase()) > -1) {
-                autocompleteItem = document.createElement('li');
-
-                // checks if the input value is in the array item (if yes, highlights it)
-                let suggestion = array[i].replace(
-                    new RegExp(value, 'gi'),
-                    '<strong>$&</strong>'
-                );
-                autocompleteItem.innerHTML = suggestion;
-                autocompleteItem.innerHTML +=
-                    "<input type='hidden' value='" + array[i] + "'>";
-
-                // adds clicked autocomplete item to input and submits the form
-                autocompleteItem.addEventListener('click', function (e) {
-                    input.value = this.getElementsByTagName('input')[0].value;
-                    closeAllLists();
-
-                    const form = input.closest('form');
-                    form?.submit();
-                });
-
-                // sorts items in the list based on if they start with the input value
-                array[i].toUpperCase().startsWith(value.toUpperCase())
-                    ? autocompleteList.insertBefore(
-                          autocompleteItem,
-                          autocompleteList.childNodes[0]
-                      )
-                    : autocompleteList.appendChild(autocompleteItem);
-            }
-        }
-    });
-
-    input?.addEventListener('keydown', function (e) {
-        // autocomplete container (for readability: 'x')
-        let x = document.querySelector(`#searchAutocompleteList`);
-        if (x) x = x.getElementsByTagName('li');
-
-        switch (e.key) {
-            case 'ArrowDown':
-                currentFocus++;
-                addActive(x);
-                break;
-            case 'ArrowUp':
-                currentFocus--;
-                addActive(x);
-                break;
-            case 'Enter':
-                if (currentFocus > -1 && x) {
-                    e.preventDefault();
-                    x[currentFocus].click();
-                }
-                closeAllLists();
-                break;
-        }
-    });
-
-    const addActive = (x) => {
-        if (!x) return false;
-        removeActive(x);
-        if (currentFocus >= x.length) currentFocus = x.length - 1;
-        if (currentFocus < 0) currentFocus = 0;
-        x[currentFocus].classList.add('autocompleteActive');
-        // makes sure the active suggestion is always visible
-        if (
-            x[currentFocus].offsetTop + x[currentFocus].offsetHeight >
-            x[0].parentNode.scrollTop + x[0].parentNode.offsetHeight
-        ) {
-            x[0].parentNode.scrollTop =
-                x[currentFocus].offsetTop +
-                x[currentFocus].offsetHeight -
-                x[0].parentNode.offsetHeight;
-        } else if (x[currentFocus].offsetTop < x[0].parentNode.scrollTop) {
-            x[0].parentNode.scrollTop = x[currentFocus].offsetTop;
-        }
-    };
-
-    const removeActive = (x) => {
-        for (let i = 0; i < x.length; i++) {
-            x[i].classList.remove('autocompleteActive');
-        }
-    };
-
-    const closeAllLists = (e) => {
-        const x = document.getElementsByClassName('autocompleteItems');
-        for (let i = 0; i < x.length; i++) {
-            if (e != x[i] && e != input) x[i].parentNode.removeChild(x[i]);
-        }
-    };
-};
-
-const searchbar = document.querySelector('#search');
-let suggestions = [];
-
-function getSuggestions() {
-    const info = document.querySelectorAll(
+const getSuggestions = () => {
+    const projectHeadings = document.querySelectorAll(
         'li.card__item.allprojects-card .header__title h3'
     );
 
-    info.forEach((item) => {
-        const modifiedContent = item.innerHTML.replace(
-            /<span\b[^>]*>(.*?)<\/span>/i,
-            ''
-        );
-        const content = modifiedContent.trim();
+    projectHeadings.forEach((heading) => {
+        const headingWithoutSpan = heading.innerHTML
+            .replace(/<span\b[^>]*>(.*?)<\/span>/i, '')
+            .trim()
+            .toLowerCase();
 
-        if (!suggestions.includes(content)) {
-            suggestions.push(content);
+        suggestions.add(headingWithoutSpan);
+    });
+};
+
+const currentSuggestions = new Set();
+const autocomplete = (e) => {
+    const inputValue = e.target.value.toLowerCase().trim();
+
+    for (const suggestion of suggestions) {
+        if (suggestion.includes(inputValue)) {
+            // if the suggestion is already in the list, just highlight it and continue
+            if (currentSuggestions.has(suggestion)) {
+                const anchor = suggestionsList.querySelector(
+                    `a[href="/search?query=${suggestion}"]`
+                );
+
+                const highlightedSuggestion = suggestion.replace(
+                    new RegExp(inputValue, 'gi'),
+                    '<strong>$&</strong>'
+                );
+                anchor.innerHTML = highlightedSuggestion;
+                continue;
+            }
+
+            currentSuggestions.add(suggestion);
+
+            const suggestionListItem = document.createElement('li');
+            const anchor = document.createElement('a');
+            anchor.setAttribute('href', `/search?query=${suggestion}`);
+            anchor.addEventListener('keydown', moveFocus);
+
+            // checks if the input value is in the array item (if yes, highlights it)
+            const highlightedSuggestion = suggestion.replace(
+                new RegExp(inputValue, 'gi'),
+                '<strong>$&</strong>'
+            );
+            anchor.innerHTML = highlightedSuggestion;
+
+            suggestionListItem.appendChild(anchor);
+
+            // sorts items in the list based on if they start with the input value
+            suggestion.startsWith(inputValue)
+                ? suggestionsList.insertBefore(
+                      suggestionListItem,
+                      suggestionsList.childNodes[0]
+                  )
+                : suggestionsList.appendChild(suggestionListItem);
+        } else {
+            currentSuggestions.delete(suggestion);
+            const suggestionAnchor = suggestionsList.querySelector(
+                `li a[href="/search?query=${suggestion}"]`
+            );
+            const suggestionListItem = suggestionAnchor?.closest('li');
+            suggestionListItem?.remove();
+        }
+    }
+};
+
+const moveFocus = (e) => {
+    const { key, currentTarget } = e;
+
+    if (key === 'ArrowDown' || key === 'ArrowUp') {
+        e.preventDefault();
+
+        const currentListItem = currentTarget.closest('li');
+
+        if (key === 'ArrowUp') {
+            const previousAnchor =
+                currentListItem.previousElementSibling?.querySelector('a');
+
+            previousAnchor ? previousAnchor.focus() : autocompleteInput.focus();
+        } else {
+            const nextAnchor =
+                currentListItem.nextElementSibling?.querySelector('a');
+
+            nextAnchor?.focus();
+        }
+    }
+};
+
+const focusFirstSuggestion = () => {
+    const firstSuggestion = suggestionsList.querySelector('a');
+
+    firstSuggestion?.focus();
+};
+
+const toggleSuggestions = (e) => {
+    if (
+        e.relatedTarget?.closest('section#searchbar') ||
+        (e.target.closest('section#searchbar') && e.type !== 'focusout')
+    ) {
+        suggestionsList.classList.remove('hide');
+    } else {
+        suggestionsList.classList.add('hide');
+    }
+};
+
+const searchSection = document.querySelector('section#searchbar');
+const autocompleteInput = searchSection?.querySelector('input#search');
+const suggestionsList = searchSection?.querySelector('.autocomplete-items');
+
+if (autocompleteInput && suggestionsList) {
+    getSuggestions();
+    autocompleteInput.addEventListener('input', autocomplete);
+
+    autocompleteInput.addEventListener('keydown', (e) => {
+        if (e.key === 'ArrowDown') {
+            e.preventDefault();
+            focusFirstSuggestion();
         }
     });
+
+    searchSection.addEventListener('focusin', toggleSuggestions);
+    searchSection.addEventListener('focusout', toggleSuggestions);
 }
-
-getSuggestions();
-
-autocomplete(searchbar, suggestions);
