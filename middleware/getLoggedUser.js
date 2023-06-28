5717573a5545c4f0672a1a308b3b1bb4d0bb54a4
Laurens256 <duinlaurens2@gmail.com>
2023-06-28 11:55:01 +0200

    add: wip profile pic partial

--- middleware/getLoggedUser.js
+++ middleware/getLoggedUser.js
@@ -1,5 +1,19 @@
-const getLoggedUser = async (req, res, next) => {    
-    res.locals.user = req.user ?? null;
+const getLoggedUser = async (req, res, next) => {
+    const user = req.user;
+    if (user) {
+        res.locals.user = {
+            ...user,
+            full_name: `
+			${user.first_name || ''} 
+			${user.insertion || ''} 
+			${user.last_name || ''}`
+			.trim()
+        };
+		console.log(res.locals.user);
+    } else {
+        res.locals.user = null;
+    }
+
     next();
 };
 
